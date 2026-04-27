03b1db2e87542709a0af544d365b37897078fd08
Mojtaba <mojtababahrami147@gmail.com>
2026-04-28 01:31:53 +0330

    Squashed commit of the following:
    commit cbd67d9b28039ff2d40e24d1aa2482aa750d5025
    Author: Mojtaba <mojtababahrami147@gmail.com>
    Date:   Mon Apr 27 23:36:38 2026 +0330
    
        stag_hunt bugfix
    
    commit 4c880c5c4221971dd5427ee3789fd8da49a3611b
    Author: Mojtaba <mojtababahrami147@gmail.com>
    Date:   Mon Apr 27 22:23:35 2026 +0330
    
        GameTheoryEnv.py finished!
    
    commit 1cbcf6a0beadd9bb93f644dda703d26d9ed79157
    Author: Mojtaba <mojtababahrami147@gmail.com>
    Date:   Mon Apr 27 20:22:45 2026 +0330
    
        Just a checkpoint
    
    commit 820c28cb0d08fb68587a54a5008c141935cba812
    Author: Mojtaba <mojtababahrami147@gmail.com>
    Date:   Fri Apr 24 13:34:22 2026 +0330
    
        preliminary files created
    
    commit 43cb797adb82b75c69945edc84ba422cf599c694
    Author: Mojtaba <mojtababahrami147@gmail.com>
    Date:   Sat Apr 18 23:59:09 2026 +0330
    
        A TorchRL class name related  bugfix

--- benchmarl/environments/__init__.py
+++ benchmarl/environments/__init__.py
@@ -12,10 +12,10 @@ from .pettingzoo.common import PettingZooClass, PettingZooTask
 from .smacv2.common import Smacv2Class, Smacv2Task
 from .vmas.common import VmasClass, VmasTask
 from .gridworld.common import GridSprayTask
-
+from .game_theory_game.common import TwoPlayerGameTheoryClass, TwoPlayerGameTheoryTask
 # The enum classes for the environments available.
 # This is the only object in this file you need to modify when adding a new environment.
-tasks = [VmasTask, Smacv2Task, PettingZooTask, MeltingPotTask, MAgentTask, GridSprayTask]
+tasks = [VmasTask, Smacv2Task, PettingZooTask, MeltingPotTask, MAgentTask, GridSprayTask, TwoPlayerGameTheoryTask]
 
 # This is a registry mapping "envname/task_name" to the EnvNameTask.TASK_NAME enum
 # It is used by automatically load task enums from yaml files.
